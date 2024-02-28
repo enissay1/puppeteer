ed70b4acda53529b8a7fa7e9c5635afce4a1bbe5
Ezzahr Yass <yassine.ezzahr@gmail.com>
2024-02-28 11:44:26 +0100

    <feat> iterer tout les pages

--- pdf.js
+++ pdf.js
@@ -14,16 +14,37 @@ const puppeteer = require('puppeteer');
     // Wait for the selector to appear on the page
     await page.waitForSelector('.slrName');
 
-    // Use page.$$eval to extract the content of each element matching the selector
-    const contentArray = await page.$$eval('.slrName', elements => {
-        // Map over the selected elements and return their inner text
-        return elements.map(element => element.innerText);
-    });
+    // Utilisez page.$$ pour obtenir un tableau de tous les éléments <li> enfants de <ul>
+    const liElements = await page.$$('#OfferListPaginationForm ul li');
 
-    // Log the extracted content
-    console.log(contentArray);
+    // Obtenez la taille du tableau (le nombre d'éléments <li>)
+    const liElementsCount = liElements.length;
 
-    // Take a screenshot for verification
+    // Définissez un tableau pour stocker les résultats de tous les pages
+    let allContentArray = [];
+
+    // Itérez sur chaque page
+    for (let i = 0; i < liElementsCount; i++) {
+        // Utilisez page.$$eval pour extraire le contenu de chaque élément .slrName sur la page actuelle
+        const contentArray = await page.$$eval('.slrName', elements => {
+            // Map over the selected elements and return their inner text
+            return elements.map(element => element.innerText);
+        });
+
+        // Ajoutez le contenu de la page actuelle au tableau global
+        allContentArray = allContentArray.concat(contentArray);
+
+        // Cliquez sur le bouton "Suivant" pour passer à la page suivante
+        await page.click('.mpNext');
+
+        // Attendez que le nouveau contenu apparaisse avant de continuer
+        await page.waitForSelector('.slrName');
+    }
+
+    // Log the extracted content from all pages
+    console.log(allContentArray);
+
+    // Take a screenshot for verification (optional)
     await page.screenshot({ path: '/home/app/example.png', fullPage: true });
 
     // Close the browser
